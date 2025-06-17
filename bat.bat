bb8480d0eee79147be9cb23a61cdbed15b86a217
souzado0800 <ninjatokyo712@gmail.com>
2025-06-17 10:57:08 -0300

    Commit inicial - projeto XStreaming

--- bat.bat
+++ bat.bat
@@ -1,30 +1,22 @@
 @echo off
 cd /d "%~dp0"
 
-REM CONFIGURAÇÃO DO GIT
-git config --global user.name "souzado0800"
-git config --global user.email "ninjatokyo712@gmail.com"
-
-REM INICIALIZAÇÃO (ignora se já existir)
+REM Inicializa o repositório
 git init
 
-REM REMOVE ORIGIN ANTIGA CASO EXISTA
-git remote remove origin 2>nul
+REM Adiciona todos os arquivos
+git add .
 
-REM ADICIONA ORIGIN NOVAMENTE
+REM Commit inicial
+git commit -m "Commit inicial - projeto XStreaming"
+
+REM Substitua a URL abaixo pela do seu repositório
 git remote add origin https://github.com/Souzado0800/XStreaming.git
 
-REM ADICIONA TODOS OS ARQUIVOS E FAZ COMMIT
-git add .
-git commit -m "Commit inicial do projeto XStreaming"
+REM Define o branch principal como master
+git branch -M master
 
-REM CRIA O BRANCH MAIN (CASO NÃO EXISTA)
-git branch -M main
-
-FORÇA CRIA
-git push origin main --force
-
-REM FAZ O PUSH
-git push -u origin main
+REM Envia para o GitHub
+git push -u origin master
 
 pause
